a8570300a18135c41f4e5aaeae5faa16ac1bd847
catelleempei <catelleempei@eaton.com>
2024-07-29 19:26:15 -0400

    cov directory fix directory synthax

--- images/windows/scripts/build/Install-CoverityClient.ps1
+++ images/windows/scripts/build/Install-CoverityClient.ps1
@@ -14,14 +14,14 @@ if (-not (Get-Command azcopy -ErrorAction SilentlyContinue)) {
     exit 1
 }
 # Use AzCopy to copy the file from Azure Blob Storage to the local directory
-azcopy copy $sourceUrl $destinationPath
-if (-not (Test-Path $destinationPath)) {
-    Write-Error "Failed to download the file."
-    exit 1
-}
+# azcopy copy $sourceUrl $destinationPath
+# if (-not (Test-Path $destinationPath)) {
+#     Write-Error "Failed to download the file."
+#     exit 1
+# }
 
 # Install the Coverity client
-Start-Process -FilePath "C:\temp/cov-analysis-win64-2024.3.1.exe" -ArgumentList "-q", "--license.region=1", "--license.agreement=agree", "--license.cov.path=""C:\temp\assets\license.dat"""
+Start-Process -FilePath "C:\temp/cov-analysis-win64-2024.3.1.exe" -ArgumentList "-q", "--license.region=1", "--license.agreement=agree", "--license.cov.path=""C:\temp\license.dat"""
 # https://eaton-my.sharepoint.com/:f:/r/personal/catelleempei_eaton_com/Documents/cov.%20license?csf=1&web=1&e=aoBUNb.dat""" -NoNewWindow -Wait
 
 #"azcopy https://stsmpdevopsimages.blob.core.windows.net/armimagelayeraddons/synopsys/coverity/cov-analysis-win64-2024.3.1.exe?sp=r&st=2024-07-08T15:07:19Z&se=2025-07-08T23:07:19Z&spr=https&sv=2022-11-02&sr=b&sig=Eme6QLzdls0MHWjvGflUtr4mGdvRmx0zPw8XeBDtLFo%3D",
