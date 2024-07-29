fd5930395ba977a5a3f4c3fed3281b79572a194f
catelleempei <catelleempei@eaton.com>
2024-07-29 19:19:14 -0400

    cov directory fix directory synthax

--- images/windows/scripts/build/Install-CoverityClient.ps1
+++ images/windows/scripts/build/Install-CoverityClient.ps1
@@ -6,9 +6,9 @@ New-Item -Path "C:\temp" -ItemType Directory -Force
 # Define the source URL and destination path
 
 #$sourceUrl="https://stsmpdevopsimages.blob.core.windows.net/armimagelayeraddons/synopsys/coverity/cov-analysis-win64-2024.3.1.exe?sp=r&st=2024-07-25T01:18:58Z&se=2024-07-25T09:18:58Z&spr=https&sv=2022-11-02&sr=b&sig=tc3aPZZir9IXAx2W0Y11rNWEsew0W28Y%2BsCwWyL%2FcyY%3De" 
-$DEVOPS_ST_SAS_TOKEN = $env:DEVOPS_ST_SAS_TOKEN
-$sourceUrl="https://stsmpdevopsimages.blob.core.windows.net/armimagelayeraddons/synopsys/coverity/cov-analysis-win64-2024.3.1.exe? + $DEVOPS_ST_SAS_TOKEN
-$destinationPath = "C:\temp\\cov-analysis-win64-2024.3.1.exe"
+# $DEVOPS_ST_SAS_TOKEN = $env:DEVOPS_ST_SAS_TOKEN
+# $sourceUrl="https://stsmpdevopsimages.blob.core.windows.net/armimagelayeraddons/synopsys/coverity/cov-analysis-win64-2024.3.1.exe? + $DEVOPS_ST_SAS_TOKEN
+$destinationPath = "C:\temp/cov-analysis-win64-2024.3.1.exe"
 if (-not (Get-Command azcopy -ErrorAction SilentlyContinue)) {
     Write-Error "AzCopy is not installed or not in the system's PATH."
     exit 1
@@ -21,7 +21,7 @@ if (-not (Test-Path $destinationPath)) {
 }
 
 # Install the Coverity client
-Start-Process -FilePath "c:\temp\\cov-analysis-win64-2024.3.1.exe" -ArgumentList "-q", "--license.region=1", "--license.agreement=agree", "--license.cov.path=""C:\temp\assets\license.dat"""
+Start-Process -FilePath "C:\temp/cov-analysis-win64-2024.3.1.exe" -ArgumentList "-q", "--license.region=1", "--license.agreement=agree", "--license.cov.path=""C:\temp\assets\license.dat"""
 # https://eaton-my.sharepoint.com/:f:/r/personal/catelleempei_eaton_com/Documents/cov.%20license?csf=1&web=1&e=aoBUNb.dat""" -NoNewWindow -Wait
 
 #"azcopy https://stsmpdevopsimages.blob.core.windows.net/armimagelayeraddons/synopsys/coverity/cov-analysis-win64-2024.3.1.exe?sp=r&st=2024-07-08T15:07:19Z&se=2025-07-08T23:07:19Z&spr=https&sv=2022-11-02&sr=b&sig=Eme6QLzdls0MHWjvGflUtr4mGdvRmx0zPw8XeBDtLFo%3D",
